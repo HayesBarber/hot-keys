baea655a941c93c38e12bb7ae1121b879c335266
Hayes Barber <you@example.com>
2024-05-27 11:38:03 -0400

    commit

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 
 const App: React.FC = () => {
     return (
-        <input type="text" autoFocus={true} className="bg-gray-900 text-white opacity-80 border border-gray-900 focus:border-white focus:outline-none focus:ring-0 rounded-xl w-[700px] h-[75px] text-2xl p-5" />
+        <input type="text" autoFocus={true} className="bg-gray-900 text-white opacity-90 border border-gray-900 focus:border-white focus:outline-none focus:ring-0 rounded-xl w-[700px] h-[75px] text-2xl p-5" />
     );
 }
 
