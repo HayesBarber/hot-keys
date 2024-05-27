26d9ba9a672c665ecb2634949f403e7291586e7a
Hayes Barber <you@example.com>
2024-05-27 19:11:32 -0400

    re-org

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, globalShortcut, Tray, Menu } from 'electron';
+import { app, BrowserWindow, globalShortcut, Tray, Menu, ipcMain } from 'electron';
 import { exec } from 'child_process';
 import { readFileSync } from 'fs';
 import Command from './command';
@@ -12,6 +12,55 @@ let commands: Record<string, Command> = {};
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
+}
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', async () => {
+  const fileContent = readFileSync('src/commands.json', 'utf-8');
+  const objects: Array<Command> = JSON.parse(fileContent);
+  console.log(objects);
+
+  objects.forEach((value) => {
+    commands[value.hotKey] = value;
+
+    globalShortcut.register(value.hotKey, () => {
+      console.log(`running ${value.command} from input ${value.hotKey}`);
+      exec(value.command);
+    });
+  });
+
+  globalShortcut.register('Command+Shift+Space', () => {
+    toggle();
+  });
+
+  buildMenu();
+
+  createWindow();
+});
+
+const buildMenu = () => {
+  const menu = Menu.buildFromTemplate([
+    { role: 'quit', label: 'Quit Commands', accelerator: 'Command+Q' },
+  ]);
+
+  if (!tray) {
+    tray = new Tray('src/icon.png');
+  }
+
+  tray.setContextMenu(menu);
+}
+
+const toggle = () => {
+  if (window.isFocused()) {
+    window.hide();
+  } else {
+    window.show();
+  }
 }
 
 const createWindow = async (): Promise<void> => {
@@ -32,55 +81,8 @@ const createWindow = async (): Promise<void> => {
   window.setVisibleOnAllWorkspaces(true);
 
   const stringify = JSON.stringify(Object.values(commands));
-  const url = `${ MAIN_WINDOW_WEBPACK_ENTRY }?commands=${ stringify }`;
+  const url = `${MAIN_WINDOW_WEBPACK_ENTRY}?commands=${stringify}`;
   await window.loadURL(url);
 
   app.dock.hide();
 };
-
-app.on('ready', async () => {
-  const fileContent = readFileSync('src/commands.json', 'utf-8');
-  const objects: Array<Command> = JSON.parse(fileContent);
-  console.log(objects);
-
-  objects.forEach((value) => {
-    commands[value.hotKey] = value;
-
-    globalShortcut.register(value.hotKey, () => {
-      console.log(`running ${ value.command } from input ${ value.hotKey }`);
-      exec(value.command);
-    });
-  });
-
-  await createWindow();
-
-  globalShortcut.register('Command+Shift+Space', () => {
-    toggle();
-  });
-
-  const menu = Menu.buildFromTemplate([
-    { role: 'quit', label: 'Quit Commands', accelerator: 'Command+Q' },
-  ]);
-
-  if (!tray) {
-    tray = new Tray('src/icon.png');
-  }
-
-  tray.setContextMenu(menu);
-});
-
-
-const toggle = () => {
-  if (window.isFocused()) {
-    window.hide();
-  } else {
-    window.show();
-  }
-}
-
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
