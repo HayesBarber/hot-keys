1b57e535c010a882a63352fc93ba6bd300f9e10f
Hayes Barber <you@example.com>
2024-05-27 20:10:28 -0400

    commit

--- src/command-prompt/app.tsx
+++ src/command-prompt/app.tsx
@@ -2,14 +2,14 @@ import Command from "../command";
 import { useState, useEffect } from "react";
 
 import {
-    Command as CommandComponent,
-    CommandEmpty,
-    CommandGroup,
-    CommandInput,
-    CommandItem,
-    CommandList,
-    CommandShortcut,
-  } from "../components/command"
+  Command as CommandComponent,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandShortcut,
+} from "../components/command"
 
 const map: Record<string, string> = {
   'Command': '⌘',
@@ -20,44 +20,50 @@ const map: Record<string, string> = {
 };
 
 const App: React.FC = () => {
-    const [commands, setCommands] = useState<Command[]>([]);
+  const [commands, setCommands] = useState<Command[]>([]);
+  const [value, setValue] = useState<string>('');
 
-    useEffect(() => {
-      const queryString = global.location.search;
-      const urlParams = new URLSearchParams(queryString);
-      const commandsParam = urlParams.get('commands');
-  
-      if (commandsParam) {
-        try {
-          const parsedCommands: Command[] = JSON.parse(decodeURIComponent(commandsParam));
-          setCommands(parsedCommands);
-        } catch (error) {
-          console.error('Failed to parse');
-        }
+  useEffect(() => {
+    const queryString = global.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const commandsParam = urlParams.get('commands');
+
+    if (commandsParam) {
+      try {
+        const parsedCommands: Command[] = JSON.parse(decodeURIComponent(commandsParam));
+        setCommands(parsedCommands);
+      } catch (error) {
+        console.error('Failed to parse');
       }
-    }, []);
+    }
+  }, []);
 
-    return (
-        <CommandComponent className="rounded-xl border">
-          <CommandInput autoFocus={true} placeholder="Search..." />
-          <CommandList>
-            <CommandEmpty>No results found.</CommandEmpty>
-            <CommandGroup heading="Hot-Keys">
-              {commands.length ? commands.map((command) => <Item displayName={command.displayName} hotKey={command.hotKey} command={command.command}/>) : <div/>}
-            </CommandGroup>
-          </CommandList>
-        </CommandComponent>
-    );
+  const onCommandSelected = (hotKey: string) => {
+    window.electronAPI.commandSelected(hotKey);
+    setValue('');
+  }
+
+  return (
+    <CommandComponent className="rounded-xl border">
+      <CommandInput autoFocus={true} value={value} onValueChange={(v: string) => setValue(v)} placeholder="Search..." />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup heading="Hot-Keys">
+          {commands.length ? commands.map((command) => <Item displayName={command.displayName} hotKey={command.hotKey} onSelect={onCommandSelected} />) : <div />}
+        </CommandGroup>
+      </CommandList>
+    </CommandComponent>
+  );
 }
 
-const Item: React.FC<Command> = ({displayName, hotKey}) => {
+const Item: React.FC<{ displayName: string, hotKey: string, onSelect: (hotKey: string) => void }> = ({ displayName, hotKey, onSelect }) => {
   const parts = hotKey.split(' ');
   parts.forEach((part, index, arr) => {
     arr[index] = map[part] ?? part;
   });
 
   return (
-    <CommandItem onSelect={() => window.electronAPI.commandSelected(hotKey)}>
+    <CommandItem onSelect={() => onSelect(hotKey)}>
       <span>{displayName}</span>
       <CommandShortcut>{parts.join('')}</CommandShortcut>
     </CommandItem>
