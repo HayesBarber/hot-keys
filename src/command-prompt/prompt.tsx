2fcf6119c718bd641f6f05dbe28f6d8c1835d7be
Hayes Barber <you@example.com>
2024-05-29 18:15:01 -0400

    add styling

--- src/command-prompt/prompt.tsx
+++ src/command-prompt/prompt.tsx
@@ -50,7 +50,7 @@ const App: React.FC = () => {
   }
 
   return (
-    <CommandComponent className="rounded-xl border">
+    <CommandComponent className="rounded-xl border outline-none focus:outline-none">
       <CommandInput autoFocus={true} onFocus={onFocus} placeholder="Search..." />
       <CommandList>
         <CommandEmpty>No results found.</CommandEmpty>
