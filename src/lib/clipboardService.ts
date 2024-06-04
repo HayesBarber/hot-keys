9aad7edb271eeb216d21dcee9901a3ef875f9da2
Hayes Barber <you@example.com>
2024-06-03 21:25:13 -0400

    more clipboard service

--- src/lib/clipboardService.ts
+++ src/lib/clipboardService.ts
@@ -1,9 +1,10 @@
 import { clipboard } from "electron";
 import { ClipboardRecord } from "../models/clipboardItem";
-import { readFileFromHomeDirectory } from "./fileUtils";
+import { readFileFromHomeDirectory, writeFileInHomeDirectory } from "./fileUtils";
 
 class ClipboardService {
     public clipboardRecords: ClipboardRecord[];
+    private clipboardFileName = 'hot-keys-pasteboard.json';
 
     constructor() {
         this.clipboardRecords = this.readClipboardFile();
@@ -11,24 +12,30 @@ class ClipboardService {
 
     public readClipboard = () => {
         const formats = clipboard.availableFormats();
-        console.log(formats);
 
         const isText = formats.includes('text/plain');
 
-        if (isText) {
-            const text = clipboard.readText().trim();
-            console.log(text);
-            return;
-        }
+        let content: string = null;
 
-        const data = clipboard.readImage();
-        if (!data.isEmpty()) {
-            console.log(data.toDataURL());
-            return;
+        if (isText) {
+            content = clipboard.readText().trim();
+        } else {
+            const data = clipboard.readImage();
+            if (!data.isEmpty()) {
+                content = data.toDataURL();
+            }
+        }
+        
+        // todo check that we don't already have this clipboard record
+        if(content) {
+            this.clipboardRecords.push({ content });
+            this.writeToClipboardFile();
         }
     };
 
-    private readClipboardFile = () => readFileFromHomeDirectory<ClipboardRecord[]>('hot-keys-pasteboard.json', []);
+    private readClipboardFile = () => readFileFromHomeDirectory<ClipboardRecord[]>(this.clipboardFileName, []);
+
+    private writeToClipboardFile = () => writeFileInHomeDirectory(this.clipboardFileName, this.clipboardRecords);
 }
 
 export default ClipboardService;
