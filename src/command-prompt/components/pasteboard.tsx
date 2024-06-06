05f9424c2d2a9a224aa8c3bb48759d1b506f43f6
Hayes Barber <you@example.com>
2024-06-06 08:01:21 -0400

    onRecordSelected

--- src/command-prompt/components/pasteboard.tsx
+++ src/command-prompt/components/pasteboard.tsx
@@ -2,87 +2,132 @@ import { ClipboardRecord } from "../../models/clipboardRecord";
 import usePasteboard from "../../hooks/usePasteboard";
 import { FooterButton, FooterMain } from "./footer";
 import useFocus from "../../hooks/useFocus";
-import { ArrowBigLeft, Copy } from "lucide-react"
+import { ArrowBigLeft, Copy } from "lucide-react";
 import {
-    Command as CommandComponent,
-    CommandList,
-    CommandGroup,
-    CommandItem,
+  Command as CommandComponent,
+  CommandList,
+  CommandGroup,
+  CommandItem,
 } from "../../components/command";
 import { useState } from "react";
 
 const Pasteboard: React.FC<{ back: () => void }> = ({ back }) => {
-    const pasteboardItems = usePasteboard();
-    const [selected, setSelected] = useState(0);
-    const ref = useFocus();
+  const pasteboardItems = usePasteboard();
+  const [selected, setSelected] = useState(0);
+  const ref = useFocus();
 
-    return (
-        <div className="window">
-            <div className="flex grow overflow-hidden">
-                <div className="w-[40%]">
-                    <CommandComponent ref={ref} value={`${selected}`} onValueChange={(value) => setSelected(parseInt(value))} className="rounded-xl outline-none focus:outline-none flex flex-col">
-                        <CommandList className="grow" >
-                            <PasteboardItems selectedIndex={selected} records={pasteboardItems} onRecordSelected={() => { }} />
-                        </CommandList>
-                    </CommandComponent>
-                </div>
-                <div className="w-[60%] my-2 border-l">
-                    <ClipboardContent records={pasteboardItems} selected={selected} />
-                </div>
-            </div>
-            <FooterMain>
-                <FooterButton onClick={() => back()} >
-                    <ArrowBigLeft className="icon" />Back
-                </FooterButton>
-                <div className="flex items-center">
-                    <FooterButton onClick={() => window.electronAPI.clearPasteboard()} >
-                        Clear pasteboard
-                    </FooterButton>
-                    <hr className="h-[20px] w-[1px] bg-border" />
-                    <FooterButton onClick={() => window.electronAPI.pasteToPasteboard()} >
-                        Add to pasteboard: ⌘⇧V
-                    </FooterButton>
-                </div>
-            </FooterMain>
+  const onRecordSelected = (record: ClipboardRecord) => {
+    window.electronAPI.copyToClipboard(record);
+  };
+
+  return (
+    <div className="window">
+      <div className="flex grow overflow-hidden">
+        <div className="w-[40%]">
+          <CommandComponent
+            ref={ref}
+            value={`${selected}`}
+            onValueChange={(value) => setSelected(parseInt(value))}
+            className="rounded-xl outline-none focus:outline-none flex flex-col"
+          >
+            <CommandList className="grow">
+              <PasteboardItems
+                selectedIndex={selected}
+                records={pasteboardItems}
+                onRecordSelected={onRecordSelected}
+              />
+            </CommandList>
+          </CommandComponent>
         </div>
-    );
+        <div className="w-[60%] my-2 border-l">
+          <ClipboardContent records={pasteboardItems} selected={selected} />
+        </div>
+      </div>
+      <FooterMain>
+        <FooterButton onClick={() => back()}>
+          <ArrowBigLeft className="icon" />
+          Back
+        </FooterButton>
+        <div className="flex items-center">
+          <FooterButton onClick={() => window.electronAPI.clearPasteboard()}>
+            Clear pasteboard
+          </FooterButton>
+          <hr className="h-[20px] w-[1px] bg-border" />
+          <FooterButton onClick={() => window.electronAPI.pasteToPasteboard()}>
+            Add to pasteboard: ⌘⇧V
+          </FooterButton>
+        </div>
+      </FooterMain>
+    </div>
+  );
 };
 
-const ClipboardContent: React.FC<{ records: ClipboardRecord[], selected: number }> = ({ records, selected }) => {
-    const hasItems = () => records.length > 0;
+const ClipboardContent: React.FC<{
+  records: ClipboardRecord[];
+  selected: number;
+}> = ({ records, selected }) => {
+  const hasItems = () => records.length > 0;
 
-    const selectedIsImage = () => {
-        // assuming hasItems has already been called
-        return records[selected].type === 'Image';
-    }
+  const selectedIsImage = () => {
+    // assuming hasItems has already been called
+    return records[selected].type === "Image";
+  };
 
-    return (
-        <div>
-            {hasItems() && records[selected].content}
-        </div>
-    );
+  return <div>{hasItems() && records[selected].content}</div>;
 };
 
-const PasteboardItems: React.FC<{ selectedIndex: number, records: ClipboardRecord[], onRecordSelected: (record: ClipboardRecord) => void }> = ({ selectedIndex, records, onRecordSelected }) => {
-    return (
-        <div>
-            <CommandGroup heading="Pasteboard">
-                {records.length ? records.map((record, i) => <PasteboardListItem key={i} index={i} isSelected={i === selectedIndex} record={record} onSelect={onRecordSelected} />) : <div />}
-            </CommandGroup>
-        </div>
-    );
+const PasteboardItems: React.FC<{
+  selectedIndex: number;
+  records: ClipboardRecord[];
+  onRecordSelected: (record: ClipboardRecord) => void;
+}> = ({ selectedIndex, records, onRecordSelected }) => {
+  return (
+    <div>
+      <CommandGroup heading="Pasteboard">
+        {records.length ? (
+          records.map((record, i) => (
+            <PasteboardListItem
+              key={i}
+              index={i}
+              isSelected={i === selectedIndex}
+              record={record}
+              onSelect={onRecordSelected}
+            />
+          ))
+        ) : (
+          <div />
+        )}
+      </CommandGroup>
+    </div>
+  );
 };
 
-const PasteboardListItem: React.FC<{ index: number, isSelected: boolean, record: ClipboardRecord, onSelect: (record: ClipboardRecord) => void }> = ({ index, isSelected, record, onSelect }) => {
-    return (
-        <CommandItem value={`${index}`} className="flex justify-between" onSelect={() => onSelect(record)}>
-            <div className="flex flex-col items-start justify-center">
-                <div>{record.type}</div>
-                <div className="text-xs text-muted-foreground">{new Date(record.timeOfCopy).toLocaleString()}</div>
-            </div>
-            {isSelected && <div className="flex items-center justify-center text-muted-foreground"><Copy className="icon" />Copy</div>}
-        </CommandItem>
-    );
+const PasteboardListItem: React.FC<{
+  index: number;
+  isSelected: boolean;
+  record: ClipboardRecord;
+  onSelect: (record: ClipboardRecord) => void;
+}> = ({ index, isSelected, record, onSelect }) => {
+  return (
+    <CommandItem
+      value={`${index}`}
+      className="flex justify-between"
+      onSelect={() => onSelect(record)}
+    >
+      <div className="flex flex-col items-start justify-center">
+        <div>{record.type}</div>
+        <div className="text-xs text-muted-foreground">
+          {new Date(record.timeOfCopy).toLocaleString()}
+        </div>
+      </div>
+      {isSelected && (
+        <div className="flex items-center justify-center text-muted-foreground">
+          <Copy className="icon" />
+          Copy
+        </div>
+      )}
+    </CommandItem>
+  );
 };
 
 export default Pasteboard;
--- src/lib/clipboardService.ts
+++ src/lib/clipboardService.ts
@@ -1,72 +1,104 @@
-import { BrowserWindow, clipboard, ipcMain, IpcMainEvent } from "electron";
+import {
+  BrowserWindow,
+  clipboard,
+  ipcMain,
+  IpcMainEvent,
+  nativeImage,
+} from "electron";
 import { ClipboardRecord } from "../models/clipboardRecord";
-import { readFileFromHomeDirectory, writeFileInHomeDirectory } from "./fileUtils";
+import {
+  readFileFromHomeDirectory,
+  writeFileInHomeDirectory,
+} from "./fileUtils";
 
 class ClipboardService {
-    public clipboardRecords: ClipboardRecord[];
-    private clipboardFileName = 'hot-keys-pasteboard.json';
+  public clipboardRecords: ClipboardRecord[];
+  private clipboardFileName = "hot-keys-pasteboard.json";
 
-    constructor(private readonly browserWindow: BrowserWindow) {
-        this.clipboardRecords = this.readClipboardFile();
-        ipcMain.on('pasteToPasteboard', () => this.readClipboard());
-        ipcMain.on('clearPasteboard', () => this.clear());
-        ipcMain.on('deletePasteboardItem', (_event: IpcMainEvent, i: number) => this.deletePasteboardItem(i));
-        ipcMain.on('copyToClipboard', (_event: IpcMainEvent, clipboardRecord: ClipboardRecord) => this.copyToClipboard(clipboardRecord));
+  constructor(private readonly browserWindow: BrowserWindow) {
+    this.clipboardRecords = this.readClipboardFile();
+    ipcMain.on("pasteToPasteboard", () => this.readClipboard());
+    ipcMain.on("clearPasteboard", () => this.clear());
+    ipcMain.on("deletePasteboardItem", (_event: IpcMainEvent, i: number) =>
+      this.deletePasteboardItem(i)
+    );
+    ipcMain.on(
+      "copyToClipboard",
+      (_event: IpcMainEvent, clipboardRecord: ClipboardRecord) =>
+        this.copyToClipboard(clipboardRecord)
+    );
+  }
+
+  public readClipboard = () => {
+    const formats = clipboard.availableFormats();
+
+    const isText = formats.includes("text/plain");
+
+    let content: string = null;
+    let timeOfCopy = Date.now();
+
+    if (isText) {
+      content = clipboard.readText().trim();
+    } else {
+      const data = clipboard.readImage();
+      if (!data.isEmpty()) {
+        content = data.toDataURL();
+      }
     }
 
-    public readClipboard = () => {
-        const formats = clipboard.availableFormats();
+    if (content) {
+      for (let i = 0; i < this.clipboardRecords.length; i++) {
+        const element = this.clipboardRecords[i];
+        if (content === element.content) return;
+      }
 
-        const isText = formats.includes('text/plain');
+      this.clipboardRecords.unshift({
+        content,
+        type: isText ? "Text" : "Image",
+        timeOfCopy,
+      });
+      this.sendThenWrite();
+    }
+  };
 
-        let content: string = null;
-        let timeOfCopy = Date.now();
+  private send = () =>
+    this.browserWindow.webContents.send(
+      "sendPasteboard",
+      this.clipboardRecords
+    );
 
-        if (isText) {
-            content = clipboard.readText().trim();
-        } else {
-            const data = clipboard.readImage();
-            if (!data.isEmpty()) {
-                content = data.toDataURL();
-            }
-        }
+  private writeToClipboardFile = () =>
+    writeFileInHomeDirectory(this.clipboardFileName, this.clipboardRecords);
 
-        if (content) {
-            for (let i = 0; i < this.clipboardRecords.length; i++) {
-                const element = this.clipboardRecords[i];
-                if (content === element.content) return;
-            }
+  private sendThenWrite = () => {
+    this.send();
+    this.writeToClipboardFile();
+  };
 
-            this.clipboardRecords.unshift({ content, type: isText ? 'Text' : 'Image', timeOfCopy });
-            this.sendThenWrite();
-        }
-    };
+  private readClipboardFile = () =>
+    readFileFromHomeDirectory<ClipboardRecord[]>(this.clipboardFileName, []);
 
-    private send = () => this.browserWindow.webContents.send('sendPasteboard', this.clipboardRecords);
+  private clear = () => {
+    this.clipboardRecords = [];
+    this.sendThenWrite();
+  };
 
-    private writeToClipboardFile = () => writeFileInHomeDirectory(this.clipboardFileName, this.clipboardRecords);
+  private deletePasteboardItem = (i: number) => {
+    if (i < 0 || i > this.clipboardRecords.length - 1) return;
 
-    private sendThenWrite = () => {
-        this.send();
-        this.writeToClipboardFile();
-    };
+    this.clipboardRecords.splice(i, 1);
+    this.sendThenWrite();
+  };
 
-    private readClipboardFile = () => readFileFromHomeDirectory<ClipboardRecord[]>(this.clipboardFileName, []);
-
-    private clear = () => {
-        this.clipboardRecords = [];
-        this.sendThenWrite();
-    };
-
-    private deletePasteboardItem = (i: number) => {
-        if(i < 0 || i > this.clipboardRecords.length - 1) return;
-
-        this.clipboardRecords.splice(i, 1);
-        this.sendThenWrite();
-    };
-
-    // todo
-    private copyToClipboard = (clipboardRecord: ClipboardRecord) => {};
+  private copyToClipboard = (clipboardRecord: ClipboardRecord) => {
+    if (clipboardRecord.type === "Text") {
+      clipboard.writeText(clipboardRecord.content);
+    } else if (clipboardRecord.type === "Image") {
+      clipboard.writeImage(
+        nativeImage.createFromDataURL(clipboardRecord.content)
+      );
+    }
+  };
 }
 
 export default ClipboardService;
