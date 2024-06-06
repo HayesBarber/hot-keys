ad476a5948af6a993e2b217c0339c922e6d1ee62
Hayes Barber <you@example.com>
2024-06-06 07:36:08 -0400

    format

--- src/lib/fileUtils.ts
+++ src/lib/fileUtils.ts
@@ -3,20 +3,20 @@ import { join } from 'path';
 import { readFileSync, writeFileSync } from 'fs';
 
 export function readFileFromHomeDirectory<T>(fileName: string, defaultValue: T) {
-    try {
-      const home = homedir();
-      const filePath = join(home, fileName);
-      const fileContent = readFileSync(filePath, 'utf-8');
-      const parsed: T = JSON.parse(fileContent);
-  
-      return parsed;
-    } catch (error) {
-      return defaultValue;
-    }
-  };
-  
-  export function writeFileInHomeDirectory(fileName: string, content: any) {
+  try {
     const home = homedir();
     const filePath = join(home, fileName);
-    writeFileSync(filePath, JSON.stringify(content), 'utf-8');
-  };
+    const fileContent = readFileSync(filePath, 'utf-8');
+    const parsed: T = JSON.parse(fileContent);
+
+    return parsed;
+  } catch (error) {
+    return defaultValue;
+  }
+};
+
+export function writeFileInHomeDirectory(fileName: string, content: any) {
+  const home = homedir();
+  const filePath = join(home, fileName);
+  writeFileSync(filePath, JSON.stringify(content), 'utf-8');
+};
