c74f52305d0dcbaa2866d3f7219e961696a01083
Hayes Barber <you@example.com>
2024-06-07 08:39:50 -0400

    change to use key

--- src/command-prompt/prompt.tsx
+++ src/command-prompt/prompt.tsx
@@ -1,10 +1,10 @@
-import useEscapeKey from "../hooks/useEscapeKey";
+import useKey from "../hooks/useKey";
 import Pasteboard from "./components/pasteboard";
 import Commands from "./components/commands";
 import useShowPasteboard from "..//hooks/useShowPasteboard";
 
 const Prompt: React.FC = () => {
-  useEscapeKey(() => window.electronAPI.hide());
+  useKey("Escape", () => window.electronAPI.hide());
   const { showPasteboard, setShowPasteboard } = useShowPasteboard();
 
   return (
