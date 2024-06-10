f79e7ced4f5ff24649ffdc91a0b51e0759d2d738
Hayes Barber <you@example.com>
2024-06-10 17:33:59 -0400

    commit

--- forge.config.ts
+++ forge.config.ts
@@ -1,35 +1,33 @@
-import type { ForgeConfig } from '@electron-forge/shared-types';
-import { MakerZIP } from '@electron-forge/maker-zip';
-import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
-import { WebpackPlugin } from '@electron-forge/plugin-webpack';
-import { FusesPlugin } from '@electron-forge/plugin-fuses';
-import { FuseV1Options, FuseVersion } from '@electron/fuses';
-import { mainConfig } from './webpack.main.config';
-import { rendererConfig } from './webpack.renderer.config';
-import { join } from 'path';
+import type { ForgeConfig } from "@electron-forge/shared-types";
+import { MakerZIP } from "@electron-forge/maker-zip";
+import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-natives";
+import { WebpackPlugin } from "@electron-forge/plugin-webpack";
+import { FusesPlugin } from "@electron-forge/plugin-fuses";
+import { FuseV1Options, FuseVersion } from "@electron/fuses";
+import { mainConfig } from "./webpack.main.config";
+import { rendererConfig } from "./webpack.renderer.config";
+import { join } from "path";
 
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
-    icon: './src/assets/icon',
+    icon: "./src/assets/icon",
     ...(process.env.SIGN
       ? {
-        osxSign: {
-          identity: process.env.APPLE_IDENTITY,
-        },
-        osxNotarize: {
-          appleId: process.env.APPLE_ID || '',
-          appleIdPassword: process.env.APPLE_PASSWORD || '',
-          teamId: process.env.APPLE_TEAM_ID || '',
-        },
-      }
+          osxSign: {
+            identity: process.env.APPLE_IDENTITY,
+          },
+          osxNotarize: {
+            appleId: process.env.APPLE_ID || "",
+            appleIdPassword: process.env.APPLE_PASSWORD || "",
+            teamId: process.env.APPLE_TEAM_ID || "",
+          },
+        }
       : {}),
-    extraResource: [
-      join(__dirname, './src/assets/tray-icon.png'),
-    ],
+    extraResource: [join(__dirname, "./src/assets/tray-icon.png")],
   },
   rebuildConfig: {},
-  makers: [new MakerZIP({}, ['darwin'])],
+  makers: [new MakerZIP({}, ["darwin"])],
   plugins: [
     new AutoUnpackNativesPlugin({}),
     new WebpackPlugin({
@@ -38,11 +36,11 @@ const config: ForgeConfig = {
         config: rendererConfig,
         entryPoints: [
           {
-            html: './src/command-prompt/index.html',
-            js: './src/command-prompt/renderer.tsx',
-            name: 'main_window',
+            html: "./src/command-prompt/index.html",
+            js: "./src/command-prompt/renderer.tsx",
+            name: "main_window",
             preload: {
-              js: './src/command-prompt/preload.ts',
+              js: "./src/command-prompt/preload.ts",
             },
           },
         ],
