5eb28f9fa751f8a92a0ace5d07cd81ea25bc1ea9
Hayes Barber <you@example.com>
2024-06-12 19:42:12 -0400

    update styles

--- src/command-prompt/components/footer.tsx
+++ src/command-prompt/components/footer.tsx
@@ -3,17 +3,23 @@ import { Button } from "../../components/button";
 import { CommandShortcut } from "../../components/command";
 
 export const FooterMain: React.FC<PropsWithChildren> = ({ children }) => {
-    return (
-        <div className="px-2">
-            <div className="flex justify-between items-center border-t h-[45px] bg-background rounded-b-xl">
-                {children}
-            </div>
-        </div>
-    );
+  return (
+    <div className="px-2">
+      <div className="flex justify-between items-center border-t h-[45px] bg-background">
+        {children}
+      </div>
+    </div>
+  );
 };
 
-export const FooterButton: React.FC<PropsWithChildren<{ onClick: () => void }>> = ({ onClick, children }) => {
-    return (
-        <Button variant="ghost" onClick={onClick}><CommandShortcut className="flex items-center">{children}</CommandShortcut></Button>
-    );
+export const FooterButton: React.FC<
+  PropsWithChildren<{ onClick: () => void }>
+> = ({ onClick, children }) => {
+  return (
+    <Button variant="ghost" onClick={onClick}>
+      <CommandShortcut className="flex items-center">
+        {children}
+      </CommandShortcut>
+    </Button>
+  );
 };
